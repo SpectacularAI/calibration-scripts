f9cf719a3b1cb5b8f822047cf00ae97fba22c509
Otto Seiskari <otto.seiskari@spectacularai.com>
2023-02-10 12:19:06 +0200

    Optionally set IMU-to-cam using the second camera as a refrence (default is the first)

--- modify-calibration.py
+++ modify-calibration.py
@@ -5,7 +5,7 @@ import numpy as np
 import json
 import sys
 
-def set_imu_to_camera_matrix(calibration, imu_to_cam):
+def set_imu_to_camera_matrix(calibration, imu_to_cam, second_camera=False):
     out = json.loads(json.dumps(calibration)) # deep copy
     out['cameras'][0]['imuToCamera'] = imu_to_cam
 
@@ -17,9 +17,15 @@ def set_imu_to_camera_matrix(calibration, imu_to_cam):
 
         # imu -> second * first -> imu
         first_to_second = np.dot(itoc2, np.linalg.inv(itoc1))
-        new_imu_to_cam_second = np.dot(first_to_second, np.array(imu_to_cam))
-
-        out['cameras'][1]['imuToCamera'] = new_imu_to_cam_second.tolist()
+        
+        if second_camera:
+            second_to_first = np.linalg.inv(first_to_second)
+            new_imu_to_cam_first = np.dot(second_to_first, np.array(imu_to_cam))
+            out['cameras'][0]['imuToCamera'] = new_imu_to_cam_first.tolist()
+            out['cameras'][1]['imuToCamera'] = imu_to_cam
+        else:
+            new_imu_to_cam_second = np.dot(first_to_second, np.array(imu_to_cam))
+            out['cameras'][1]['imuToCamera'] = new_imu_to_cam_second.tolist()
 
     return out
 
@@ -32,9 +38,12 @@ if __name__ == '__main__':
         help='set IMU to camera matrix, keeping stereo camera calibration intact')
     set_imu_to_cam.add_argument('imu_to_camera_matrix',
         help='for example: [[1,0,0,0.1],[0,1,0,0.2],[0,0,1,0.3],[0,0,0,1]]')
+    set_imu_to_cam.add_argument('--second', action='store_true',
+        help='the given IMU-to-cam matrix is for the second camera')
 
     args = p.parse_args()
     if hasattr(args, 'imu_to_camera_matrix'):
         print(json.dumps(set_imu_to_camera_matrix(
             json.load(sys.stdin),
-            json.loads(args.imu_to_camera_matrix)), indent=2))
+            json.loads(args.imu_to_camera_matrix),
+            args.second), indent=2))
